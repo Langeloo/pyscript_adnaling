ad2a53c3979d4362427ae6a175ceb03e34e71a09
Langeloo <73621548+Langeloo@users.noreply.github.com>
2022-06-16 02:14:34 -0500

    include data and tables

--- aling.py
+++ aling.py
@@ -1,9 +1,4 @@
-# alineamiento de secuencias
-#from pyodide import create_proxy
-#console.log("Iniciando alineamiento de secuencias")
 
-#seq1 = Element("first-sequence").element.value.replace(" ", "").upper()
-#seq2 = Element("second-sequence").element.value.replace(" ", "").upper()
 sequence1 = Element("first-sequence")
 sequence2 = Element("second-sequence")
 global_button = Element('global_aling')
@@ -16,45 +11,21 @@ alert_seq1 = Element('alert_seq1')
 alert2_seq1 = Element('alert2_seq1')
 alert_seq2 = Element('alert_seq2')
 alert2_seq2 = Element('alert2_seq2')
+table_head = Element('table_head')
+table_body = Element('table_body')
+table_footer = Element('table_footer')
+table_head_local = Element('table_head_local')
+table_body_local = Element('table_body_local')
+table_footer_local = Element('table_footer_local')
+local_data = Element("result_local")
+global_data = Element("result_global")
+global_similarity = Element("global_similarity")
+local_similarity = Element("local_similarity")
+len1_seq1 = Element('len1_seq1')
+len1_seq2 = Element('len1_seq2')
+len2_seq1 = Element('len2_seq1')
+len2_seq2 = Element('len2_seq2')
 
-#lower = Element("lower").element.value
-#upper = Element("upper").element.value
-#lower2 = Element("lower2").element.value
-#upper2 = Element("upper2").element.value
-
-
-result_p = Element("result")
-
-
-result = []
-#sec_zip = zip(seq1, seq2)
-#print(seq1)
-#print(seq2)
-#print(list(sec_zip))
-
-
-
-class bcolors:
-    OK = "\033[92m"  # GREEN
-    WARNING = "\033[93m"  # YELLOW
-    FAIL = "\033[91m"  # RED
-    RESET = "\033[0m"  # RESET COLOR
-
-
-caracters_error = (
-    bcolors.OK
-    + "Error: La secuencia solo debe tener caracteres ACTG"
-    + bcolors.RESET
-)
-
-
-def verify_caracters():
-    for i, j in sec_zip:
-        if i not in "ACTG" or j not in "ACTG":
-            alert_seq1.write(caracters_error)
-            return False
-        else:
-            return True
         
 def ver_text(sequence1, sequence2):
     #sequence1
@@ -94,11 +65,9 @@ def ver_text(sequence1, sequence2):
         alert_seq2.write('OK')
         
     if flagco_seq2:
-        #print(alert2_seq2.element.value)
-        pass
+        alert2_seq2.write('Error: La secuencia debe tener entre 40 y 60 caracteres')
     else:
-        #print(alert2_seq2.element.value)
-        pass
+        alert2_seq2.write('OK')
         
     #alerts
     if flagca_seq1 or flagco_seq1 or flagca_seq2 or flagco_seq2:
@@ -121,44 +90,8 @@ def ver_text(sequence1, sequence2):
         slidermin2.element.max = len(sequence2)
         slidermax2.element.max = len(sequence2)
         slidermax2.element.value = len(sequence2)
+        
     
-
-
-def global_aling():
-        equal = True if len(sequence1.element.value) == len(sequence2.element.value) else False
-        
-        
-        if equal:
-            print(bcolors.OK + "Las secuencias son iguales" + bcolors.RESET)
-            # return True
-
-
-
-def local_aling(seq1_init, seq2_init, seq1_final, seq2_final):
-    if verify_caracters():
-        seq1_local = seq1[seq1_init - 1 : seq1_final - 1]
-        seq2_local = seq2[seq2_init - 1 : seq2_final - 1]
-        seq_zip = zip(seq1_local, seq2_local)
-        equal = True if seq1_final - seq1_init == seq2_final - seq2_init else False
-        return aling(seq_zip, equal)
-    else:
-        return caracters_error
-
-
-def aling(seq_zip, equal):
-    for i, j in seq_zip:
-        result.append(True) if i == j else result.append(False)
-    if equal:
-        similarity = (
-            "La similitud es: "
-            + ((result.count("1") / len(result)) * 100).round(2)
-            + "%"
-        )
-    else:
-        similarity = "No es posible hallar el porcentaje de similitud, cadenas de diferente longitud"
-
-    return {"seqs": sec_zip, "result": result, "similarity": similarity}
-
 #input_event()
 def input_event(evt=None):
     ver_text(sequence1.element.value.replace(" ", "").upper(), sequence2.element.value.replace(" ", "").upper())
@@ -166,19 +99,88 @@ def input_event(evt=None):
 sequence1.element.onkeypress = input_event
 sequence2.element.onkeypress = input_event
 
-
+def aling(secu1, secu2, equal):
+    pass
 
 #button global aling
 def gl_aling(*args, **kws):
-    #local_button.element.disabled = True
-    slidermin1.element.max = 90
-    slidermax1.element.max = 90
-    #print(seq1)
+    result = []
+    similarity = ""
+    str_seq1 = sequence1.element.value.replace(" ", "").upper()
+    str_seq2 = sequence2.element.value.replace(" ", "").upper()
+    str_table_head = ""
+    str_table_body = ""
+    str_table_footer = ""
+    equal = True if len(str_seq1) == len(str_seq2) else False
+    seq_zip = zip(str_seq1, str_seq2)
+    for i,  j in seq_zip:
+        if i != j:
+            result.append(False)
+        else:
+            result.append(True)
+            
+        str_table_head += "<th>" + i + "</th>"
+        str_table_footer += "<th>" + j + "</th>"
+    for i in result:
+        if i:
+            str_table_body += "<td class='bg-success'>" + "1" + "</td>"
+        else:
+            str_table_body += "<td class='bg-danger'>" + "0" + "</td>"
+            
+    if equal:
+        similarity = (
+            "La similitud es: "
+            + str((result.count(True) / max(len(str_seq1),len(str_seq2))) * 100)
+            + "%"
+        )
+    else:
+        similarity = "No es posible hallar el porcentaje de similitud, cadenas de diferente longitud"
+
+    table_head.write(str_table_head)
+    table_body.write(str_table_body)
+    table_footer.write(str_table_footer)
+    global_similarity.write(similarity)
+    len1_seq1.write("Longitud sequencia 1: " + str(len(str_seq1)))
+    len1_seq2.write("Longitud sequencia 2: " + str(len(str_seq2)))
     
     
 #button local aling
 def lc_aling(*args, **kws):
-    #global_button.element.disabled = True
-    #print(seq2, upper, lower, upper2, lower2)
-    result_p.write('Element write')
-    
+    similarity = ""
+    result = []
+    str_seq1 = sequence1.element.value.replace(" ", "").upper()[int(slidermin1.element.value) - 1 : int(slidermax1.element.value) - 1]
+    str_seq2 = sequence2.element.value.replace(" ", "").upper()[int(slidermin2.element.value) - 1 : int(slidermax2.element.value) - 1]
+    str_table_head = ""
+    str_table_body = ""
+    str_table_footer = ""
+    equal = True if len(str_seq1) == len(str_seq2) else False
+    seq_zip = zip(str_seq1, str_seq2)
+    for i,  j in seq_zip:
+        if i != j:
+            result.append(False)
+        else:
+            result.append(True)
+            
+        str_table_head += "<th>" + i + "</th>"
+        str_table_footer += "<th>" + j + "</th>"
+    for i in result:
+        if i:
+            str_table_body += "<td class='bg-success'>" + "1" + "</td>"
+        else:
+            str_table_body += "<td class='bg-danger'>" + "0" + "</td>"
+            
+    if equal:
+        similarity = (
+            "La similitud es: "
+            + str((result.count(True) / max(len(str_seq1),len(str_seq2))) * 100)
+            + "%"
+        )
+    else:
+        similarity = "No es posible hallar el porcentaje de similitud, cadenas de diferente longitud"
+
+    table_head_local.write(str_table_head)
+    table_body_local.write(str_table_body)
+    table_footer_local.write(str_table_footer)
+    local_similarity.write(similarity)
+    len2_seq1.write("Longitud sequencia 1: " + str(len(str_seq1)))
+    len2_seq2.write("Longitud sequencia 2: " + str(len(str_seq2)))
