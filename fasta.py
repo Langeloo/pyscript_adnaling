cdcdc5a13c13f9849a338f25a793a17a0cc0014f
Langeloo <miguelitoramirez02.2@gmail.com>
2022-07-25 02:23:00 -0500

    add input event in upload file

--- fasta.py
+++ fasta.py
@@ -1,11 +1,26 @@
 #DOM Elements
-from multiprocessing.connection import wait
-
-
 fasta_1 = Element("fasta_1")
 fasta_2 = Element("fasta_2")
 
 #button fasta action
-def fasta_action(*args, **kws):
-    print("Action fasta event")
+global_aling_fasta = Element("global_aling_fasta")
+local_aling_fasta = Element("local_aling_fasta")
 
+def input_event(evt):
+    print("input_event upload fasta")
+    document = fasta_1.element.files[0]
+    print(document)
+    #document = evt.target.files[0]
+    #with open(document, "r") as f:
+        #txtread = f.read()
+        #print(document)
+    print(fasta_1.element.value)
+    
+fasta_1.element.onchange = input_event
+fasta_2.element.onchange = input_event
+
+def gl_aling_fasta(*args, **kws):
+    print("gl_aling_fasta")
+    
+def lc_aling_fasta(*args, **kws):
+    print("lc_aling_fasta")
